cf9a6630730e889b72d961f74a2f118dc0187d81
Kristian3333 <kristiangbarman@gmail.com>
2024-05-20 19:24:42 +0200

    'totally_new_approach'

---
+++ api/posts.js
@@ -0,0 +1,31 @@
+// api/posts.js
+const express = require('express');
+const dbConnect = require('../lib/dbConnect');
+const Post = require('../models/Post');
+
+const router = express.Router();
+
+router.get('/api/posts', async (req, res) => {
+    await dbConnect();
+    try {
+        const posts = await Post.find();
+        res.status(200).json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error retrieving posts');
+    }
+});
+
+router.post('/api/posts', async (req, res) => {
+    await dbConnect();
+    try {
+        const newPost = new Post(req.body);
+        await newPost.save();
+        res.status(201).send('Post created');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error saving post');
+    }
+});
+
+module.exports = router;
--- index.js
+++ index.js
@@ -1,41 +1,13 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('dotenv').config({ path: '.env.local' });
-const Post = require('./models/Post');
-
 const app = express();
-app.set('view engine', 'ejs');
 
+app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-mongoose.connect(process.env.MONGODB_URI)
-    .then(() => console.log("Connected to MongoDB"))
-    .catch(err => console.error("Failed to connect to MongoDB", err));
-
-// Route to display the form and list posts
-app.get('/', async (req, res, next) => {
-    try {
-        const posts = await Post.find();
-        res.render('posts', { posts });
-    } catch (error) {
-        next(error);
-    }
-});
-
-app.post('/', async (req, res, next) => {
-    try {
-        const newPost = new Post(req.body);
-        await newPost.save();
-        res.redirect('/');
-    } catch (error) {
-        next(error);
-    }
-});
-
-// Error handling middleware
+ 
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
  
