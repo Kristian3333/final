173a33d52f034c1ae0666f45e4cb6758e5bf7bf5
Kristian3333 <kristiangbarman@gmail.com>
2024-05-20 19:27:18 +0200

    'totallynew'

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Configure your middleware, routes, etc., as usual
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
- 
+
+// Error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
 
- 
+// Export the app instance for Vercel to use
+module.exports = app;
